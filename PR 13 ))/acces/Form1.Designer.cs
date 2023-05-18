7a547aa810ab9dcd6ec9aaa881b034cbb9eb7ed3
SulCool <125337911+SulCool@users.noreply.github.com>
2023-05-18 13:27:34 +0300

    Add files via upload
    PR13

---
+++ PR 13 ))/acces/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace acces
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.idi = new System.Windows.Forms.Label();
+            this.neme = new System.Windows.Forms.Label();
+            this.nini = new System.Windows.Forms.Label();
+            this.INSERT = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.del = new System.Windows.Forms.Button();
+            this.dta1 = new System.Windows.Forms.DataGridView();
+            this.IDDD = new System.Windows.Forms.TextBox();
+            this.nnm = new System.Windows.Forms.TextBox();
+            this.newnnm = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dta1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // idi
+            // 
+            this.idi.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.idi.AutoSize = true;
+            this.idi.Location = new System.Drawing.Point(50, 38);
+            this.idi.Name = "idi";
+            this.idi.Size = new System.Drawing.Size(18, 13);
+            this.idi.TabIndex = 0;
+            this.idi.Text = "ID";
+            // 
+            // neme
+            // 
+            this.neme.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.neme.AutoSize = true;
+            this.neme.Location = new System.Drawing.Point(50, 61);
+            this.neme.Name = "neme";
+            this.neme.Size = new System.Drawing.Size(35, 13);
+            this.neme.TabIndex = 0;
+            this.neme.Text = "Name";
+            // 
+            // nini
+            // 
+            this.nini.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.nini.AutoSize = true;
+            this.nini.Location = new System.Drawing.Point(50, 85);
+            this.nini.Name = "nini";
+            this.nini.Size = new System.Drawing.Size(55, 13);
+            this.nini.TabIndex = 0;
+            this.nini.Text = "Newname";
+            // 
+            // INSERT
+            // 
+            this.INSERT.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.INSERT.Location = new System.Drawing.Point(327, 51);
+            this.INSERT.Name = "INSERT";
+            this.INSERT.Size = new System.Drawing.Size(75, 23);
+            this.INSERT.TabIndex = 2;
+            this.INSERT.Text = "INSERT";
+            this.INSERT.UseVisualStyleBackColor = true;
+            this.INSERT.Click += new System.EventHandler(this.INSERT_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button2.Location = new System.Drawing.Point(441, 51);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "UPDATE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // del
+            // 
+            this.del.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.del.Location = new System.Drawing.Point(552, 51);
+            this.del.Name = "del";
+            this.del.Size = new System.Drawing.Size(75, 23);
+            this.del.TabIndex = 2;
+            this.del.Text = "DELETE";
+            this.del.UseVisualStyleBackColor = true;
+            this.del.Click += new System.EventHandler(this.del_Click);
+            // 
+            // dta1
+            // 
+            this.dta1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.dta1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dta1.Location = new System.Drawing.Point(12, 121);
+            this.dta1.Name = "dta1";
+            this.dta1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dta1.Size = new System.Drawing.Size(783, 317);
+            this.dta1.TabIndex = 3;
+            this.dta1.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dta1_CellEnter);
+            // 
+            // IDDD
+            // 
+            this.IDDD.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.IDDD.Location = new System.Drawing.Point(111, 35);
+            this.IDDD.Name = "IDDD";
+            this.IDDD.Size = new System.Drawing.Size(154, 20);
+            this.IDDD.TabIndex = 4;
+            // 
+            // nnm
+            // 
+            this.nnm.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.nnm.Location = new System.Drawing.Point(111, 58);
+            this.nnm.Name = "nnm";
+            this.nnm.Size = new System.Drawing.Size(154, 20);
+            this.nnm.TabIndex = 5;
+            // 
+            // newnnm
+            // 
+            this.newnnm.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.newnnm.Location = new System.Drawing.Point(111, 82);
+            this.newnnm.Name = "newnnm";
+            this.newnnm.Size = new System.Drawing.Size(154, 20);
+            this.newnnm.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.newnnm);
+            this.Controls.Add(this.nnm);
+            this.Controls.Add(this.IDDD);
+            this.Controls.Add(this.dta1);
+            this.Controls.Add(this.del);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.INSERT);
+            this.Controls.Add(this.nini);
+            this.Controls.Add(this.neme);
+            this.Controls.Add(this.idi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dta1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label idi;
+        private System.Windows.Forms.Label neme;
+        private System.Windows.Forms.Label nini;
+        private System.Windows.Forms.Button INSERT;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button del;
+        private System.Windows.Forms.DataGridView dta1;
+        private System.Windows.Forms.TextBox IDDD;
+        private System.Windows.Forms.TextBox nnm;
+        private System.Windows.Forms.TextBox newnnm;
+    }
+}
+
---
+++ PR 13 ))/acces/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+
+namespace acces
+{
+    public partial class Form1 : Form
+    {
+        OleDbConnection con;
+        OleDbDataAdapter da;
+        OleDbCommand cmd;
+        DataSet ds;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Getstud()
+        {
+            
+                con = new OleDbConnection("Provider=Microsoft.ACE.Oledb.12.0;Data Source=student.accdb");
+                da = new OleDbDataAdapter("SELECT *FROM student", con);
+                ds = new DataSet();
+                con.Open();
+                da.Fill(ds, "student");
+                dta1.DataSource = ds.Tables["student"];
+                con.Close();
+            
+        
+
+        }
+
+        private void INSERT_Click(object sender, EventArgs e)
+        {
+            string query = "Insert into student (Name,Newname) values (@fName,@lName)";
+            cmd = new OleDbCommand(query, con);
+            cmd.Parameters.AddWithValue("@fName", nnm.Text);
+            cmd.Parameters.AddWithValue("@lName", newnnm.Text);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Getstud();
+
+        }
+
+        private void del_Click(object sender, EventArgs e)
+        {
+            string query = "Delete From Student Where ID=@id";
+            cmd = new OleDbCommand(query, con);
+            cmd.Parameters.AddWithValue("@id", dta1.CurrentRow.Cells[0].Value);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Getstud();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string query = "Update student Set Name=@fName,Newname=@lName Where ID=@id";
+            cmd = new OleDbCommand(query, con);
+            cmd.Parameters.AddWithValue("@ad", nnm.Text);
+            cmd.Parameters.AddWithValue("@soyad", newnnm.Text);
+            cmd.Parameters.AddWithValue("@id", Convert.ToInt32(IDDD.Text));
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Getstud();
+
+        }
+
+        private void dta1_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            IDDD.Text = dta1.CurrentRow.Cells[0].Value.ToString();
+            nnm.Text = dta1.CurrentRow.Cells[1].Value.ToString();
+            newnnm.Text = dta1.CurrentRow.Cells[2].Value.ToString();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Getstud();
+        }
+    }
+
+}
